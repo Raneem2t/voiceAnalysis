cdfda6715ae9b5afba49aa5b926ea2a774e26646
Raneem <dev.raneem@gmail.com>
2018-09-11 21:02:26 +0300

    ..

--- speechToText.py
+++ speechToText.py
@@ -1,11 +1,13 @@
 
 import requests
-
+import gui
 
 APP_ACCESS_TOKEN = 'P637HVIWSHN73DKHIB2SPGVTKLVRZWSI'
 
 base_url = 'https://api.wit.ai/speech'
 
+
+# gui_file = gui
 
 def read_audio(file_path):
     # function to read audio(wav) file
@@ -18,13 +20,15 @@ def read_audio(file_path):
 
 
 def convert_speech_to_text(self):
+    # file gui_file.answer
     file = "/Users/raneem/Desktop/voice/2.wav"
+    # file = "/Users/raneem/Desktop/voice/01_samples_trimmed_noise_reduced/04_voices_org.wav"
          # file = tryGui.path_name
     audio = read_audio(file)
     headers = {'authorization': 'Bearer ' + APP_ACCESS_TOKEN,
                'Content-Type': 'audio/wav'}
     data_response = requests.post(base_url, headers=headers, data=audio).json()
-     # self.find_criminal_words(wordList)
+    # self.find_criminal_words(wordList)
 
         # print(data_response['_text']) # Print the text
 
@@ -45,12 +49,12 @@ def spilt_into_words(self):
 
 
 
-if __name__== '__main__':
-    speech_to_Text = speechToText()
-    # database_connection.create_table()       # create table
-    # database_connection.insert_new_record()  # insert new record
-    # database_connection.query_all()          # display all records
-    # database_connection.update_record()      # update record
-    # database_connection.drop_table()         # delete table
-    # database_connection.audio_to_text()
-    speech_to_Text.spilt_into_words()
+# if __name__== '__main__':
+#     speech_to_Text = speechToText()
+#     # database_connection.create_table()       # create table
+#     # database_connection.insert_new_record()  # insert new record
+#     # database_connection.query_all()          # display all records
+#     # database_connection.update_record()      # update record
+#     # database_connection.drop_table()         # delete table
+#     # database_connection.audio_to_text()
+#     speech_to_Text.spilt_into_words()
