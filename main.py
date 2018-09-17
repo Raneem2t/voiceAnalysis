862178822d9a08df4cd7b77f07c5e44b04884a39
Raneem <dev.raneem@gmail.com>
2018-09-17 04:36:40 +0300

    ..

---
+++ main.py
@@ -0,0 +1,15 @@
+import EmotionSpeech
+import CriminalWords
+
+
+if __name__ == '__main__':
+
+    c= CriminalWords.CriminalWords()
+
+    e= EmotionSpeech.EmotionState()
+
+
+    e.EmotionState()
+    c.find_criminal_words()
+
+
