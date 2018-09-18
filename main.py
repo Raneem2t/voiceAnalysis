29cd5ed5f4a0137f132f826eab0c412cadf52887
Raneem <dev.raneem@gmail.com>
2018-09-18 19:26:48 +0300

    ..

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import EmotionSpeech
 import CriminalWords
+import timeit
+
 
 
 if __name__ == '__main__':
 
+    start = timeit.default_timer()
+
     c= CriminalWords.CriminalWords()
-
     e= EmotionSpeech.EmotionState()
-
 
     e.EmotionState()
     c.find_criminal_words()
+    c.find_abnormalWord()
 
 
+    stop = timeit.default_timer()
+
+    print('Time: ', stop - start)
