295de0295256c50f3c23eaff16aee9e64558691d
Raneem <dev.raneem@gmail.com>
2018-09-18 19:27:37 +0300

    ..

--- ConvertAudio.py
+++ ConvertAudio.py
@@ -3,13 +3,11 @@ from pydub import AudioSegment
 
 def convert(file_path):
 
-    src = file_path
-    dst = "test.wav"
+    path = file_path
+    new_path = "audio_file/test.wav"
 
     # convert wav to mp3
-    sound = AudioSegment.from_mp3(src)
-    sound.export(dst, format="wav")
-
-    new_path = "test.wav"
+    sound = AudioSegment.from_mp3(path)
+    sound.export(new_path, format="wav")
 
     return new_path
