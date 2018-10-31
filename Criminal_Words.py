3407166620bc78ab19b76d0f4d86c21f41c29a29
Raneem <32183535+Raneem2t@users.noreply.github.com>
2018-10-31 04:02:17 +0300

    Add files via upload

--- Criminal_Words.py
+++ Criminal_Words.py
@@ -1,64 +1,129 @@
 
 import psycopg2
-import speechToText
+import re
+import csv
 
 # Connect to the database
-def __init__(self):
-    try:
-        self.conniction = psycopg2.connect("dbname= 'postgres' user='raneem' host='localhost' password='123456' port='5432'")
-        self.conniction.autocommit = True
-        self.cursor = self.conniction.cursor()
+# def __init__(self):
+#     try:
+#         self.conniction = psycopg2.connect("dbname= 'postgres' user='raneem' host='localhost' password='123456' port='5432'")
+#         self.conniction.autocommit = True
+#         self.cursor = self.conniction.cursor()
+#
+#     except:
+#         print("Cannot connect to database")
+#
+#
+#
+# def find_criminal_words(text):
+#
+#     text = text
+#     # print(text)
+#     # self.obj.gu(text)
+#     cr= []
+#     # Spilt text into words and
+#     # Skip some unusual words
+#     try:
+#         skipwords = ['what', 'who', 'is', 'a', 'at', 'is', 'he', "are", "the", "so", "to", "for", "if", "where", "was",
+#                      "ware", "by"]
+#
+#         wordList = text.split(" ")  # split text into words
+#
+#         resultwords = [word for word in wordList if word.lower() not in skipwords]
+#         result = ' '.join(resultwords)
+#
+#         print(result)
+#         conn = ""
+#         try:
+#             conn = psycopg2.connect("dbname= 'postgres' user='raneem' host='localhost' password='123' port='5432'")
+#         except:
+#             print ("I am unable to connect to the database.")
+#
+#         cur = conn.cursor()
+#
+#         # detect the criminal words
+#
+#         try:
+#             print("The criminal words were detected are: ...")
+#             for i in range(len(resultwords)):
+#
+#                 cur.execute("SELECT name FROM word WHERE name = '%s'" % resultwords[i])
+#                 # print("2")
+#                 for row in cur:
+#                     row == ""
+#                     cr.append(row)
+#
+#                     # print(row)
+#
+#             f = "".join(str(e) for e in cr)
+#             res = re.sub('\ |\(|\.|\)|\/|\'|\:', '', f)
+#
+#         except:
+#             res = "No criminal words detect..."
+#             print("No criminal words detect...")
+#
+#
+#
+#     except:
+#         res = "Error"
+#         print("Error")
+#
+#     print(res)
+#     return res
 
-    except:
-        print("Cannot connect to database")
+def find_criminal_words(text):
 
+        text = text
+        cr = []
 
-
-def find_criminal_words():
-
-    text = speechToText.convert_speech_to_text()
-    print(text)
-    # self.obj.gu(text)
-    cr= []
-    # Spilt text into words and
-    # Skip some unusual words
-    try:
-        skipwords = ['what', 'who', 'is', 'a', 'at', 'is', 'he', "are", "the", "so", "to", "for", "if", "where", "was",
-                     "ware", "by"]
-        wordList = text.split(" ")  # split text into words
-
-        resultwords = [word for word in wordList if word.lower() not in skipwords]
-        result = ' '.join(resultwords)
-
-        # print(result)
-        conn = ""
         try:
-            conn = psycopg2.connect("dbname= 'postgres' user='raneem' host='localhost' password='123456' port='5432'")
+            skipwords = ['what', 'who', 'is', 'a', 'at', 'is', 'he', "are", "the", "so", "to", "for", "if", "where",
+                         "was", "ware", "by"]
+
+            wordList = text.split(" ")  # split text into words
+
+            resultwords = [word for word in wordList if word.lower() not in skipwords]
+            result = ' '.join(resultwords)
+
+            # print(result)
         except:
-            print ("I am unable to connect to the database.")
-
-        cur = conn.cursor()
-
-        # detect the criminal words
+            res = "Error"
+            print("Error")
 
         try:
             print("The criminal words were detected are: ...")
-            for i in range(len(wordList)):
 
-                cur.cursor.execute("SELECT name FROM pet WHERE name = '%s'" % wordList[i])
+            with open('test.csv', 'rt') as f:
+                reader = csv.reader(f, delimiter=',')
 
-                for row in cur.cursor:
-                    row == ""
-                    cr = row
+                for row in reader:
+                    for field in row:
+                        for w in wordList:
+                            if field == w:
 
-                    # self.obj.gu(row)
+                                cr.append(field)
+                                print(field)
 
-                    print(row)
-                    return cr
+
+            res = " ".join(str(e) for e in cr)
+            # res = re.sub('\ |\(|\.|\)|\/|\'|\:', ' ', f)
+
         except:
+            res = "No criminal words detect..."
             print("No criminal words detect...")
 
 
-    except:
-        print("Error")
-    return cr
+
+        print(res)
+        return res
+
+
+
+
+if __name__ == '__main__':
+
+    c= find_criminal_words("hi Hi Hey")
+    print(c)
+
+
+
